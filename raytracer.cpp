16482866fe84701ee4a1f39822b2967c4a2cbed1
Zhongzhi Li <lizhongzhi000@qq.com>
2017-03-16 13:10:51 -0700

    raytracer

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,6 +1,44 @@
 #include "Raytracer.h"
 
 Raytracer:: Color trace(Ray r,int depth){
+    typedef vector<Shape *>::iterator shape_itr;
     
+    Color RayTracer::trace(Ray r, int level)
+    {
+        Color black = Color(0, 0, 0);
+        
+        if (level == 0) return black;
+        
+        shape_itr it = scene->getShapes()->begin();
+        shape_itr end = scene->getShapes()->end();
+        
+        vec3 pos;
+        double dist;
+        double closestDist = -1;
+        vec3 closestPos;
+        Shape *closestShape = NULL;
+        
+        for ( ; it != end; it ++) {
+            pos = (*it)->intersect(r);
+            if (pos != NULL) {
+                dist = (r.getPos() - pos).length();
+                if (closestDist < 0 || dist < closestDist) {
+                    closestDist = dist;
+                    closestPos = pos;
+                    closestShape = *it;
+                }
+            }
+        }
+        
+        if (closestShape != NULL) {
+            vec3 theNormal = closestShape->calculateNormal(closestPos);
+            vec3 vpar = (theNormal * r.getDir().normalize()) * theNormal;
+            vec3 ref = (r.getDir() - 2*vpar).normalize();
+            return closestShape->hit(closestPos) + closestShape->getSpecular() * trace(Ray(closestPos + 0.01 * ref, ref), level-1);
+        }
+        
+        return black;
+    }
+
 
 }
